45654a0987e720a1fa8537a4f78118011a343add
Jhosselyn30 <astridjhosselyn@gmail.com>
2024-05-30 14:01:41 -0500

    2do commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'primary':'#f3c614',
+        'secondary':'#353535'
+      },
+      height:{
+        '80':'20rem',
+        '100':'25rem'
+      }
+    },
+  },
+  variants: {
+    opacity:['responsive','hover','focus','disabled'],
+  },
+  plugins: [],
+}
+
